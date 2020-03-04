cf8cfd933df706719462c2a89e93e112139b3434
Kim Mugyum <gyum2726@naver.com>
2020-03-04 19:50:45 +0900

    server연결과정

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+var mysql = require('mysql');
+const port = 3000
+
+var db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password: '1234',
+    database:'assignment'
+});
+
+db.connect();
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/menu', function(req, res){
+    db.query(`SELECT * FROM catagori`, function(error, result)
+    {
+        if(error)
+        {
+            console.log(error)
+        }
+        console.log(result)
+        res.json(result)
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ jquery.js
@@ -0,0 +1,27 @@
+
+getdata()
+
+function getdata()
+{
+    $.ajax({
+        url: 'http://localhost:3000/menu',
+        type : 'GET',
+        success:function(data){
+            for(var i = 0; i < data.length; i++)
+            {
+                menu(data[i])
+            }
+        }
+    })
+}
+
+
+function menu(data)
+{
+    var ul = document.getElementById("menu") //첫번째
+    var li = document.createElement("li")
+    li.setAttribute("id", data.id)
+    var value = document.createTextNode(data.name)
+    li.appendChild(value)
+    ul.appendChild(li)
+}
