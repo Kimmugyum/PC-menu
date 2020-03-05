b9897fa3c81e72a4a020272cefda3c987d31a1f0
Kim Mugyum <gyum2726@naver.com>
2020-03-05 10:04:17 +0900

    html 수정 및 jquery 에러

--- public/jquery.js
+++ public/jquery.js
@@ -14,13 +14,17 @@ function getdata()
     })
 }
 
+$(function(data)
+{   
+    $("#menu").append(<li id="${data.id}">${data.name}</li>);
+})
 
-function menu(data)
-{
-    var ul = document.getElementById("menu") //첫번째
-    var li = document.createElement("li")
-    li.setAttribute("id", data.id)
-    var value = document.createTextNode(data.name)
-    li.appendChild(value)
-    ul.appendChild(li)
-}
+
+// function menu(data) javscript 작업한거
+// {
+//     var ul = document.getElementById("menu") //첫번째
+//     var li = document.createElement("li")
+//     li.setAttribute("id", data.id)
+//     var value = document.createTextNode(data.name)
+//     li.appendChild(value)
+//     ul.appendChild(li)
