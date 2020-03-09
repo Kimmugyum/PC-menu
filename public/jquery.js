8853d1f8b6867918ad9c7ae29f06b55b456ba9ef
Kim Mugyum <gyum2726@naver.com>
2020-03-09 16:04:18 +0900

    jquery 대소문자 구분 및 var a 변수 제거, menuPicture함수수정

--- public/jquery.js
+++ public/jquery.js
@@ -1,6 +1,6 @@
-getdata()
+getData()
 
-function getdata()
+function getData()
 {
     $.ajax({
         url: 'http://localhost:3000/menu',
@@ -10,18 +10,22 @@ function getdata()
             {
                 menu(data[i])
             }
+            $("#menu li").click(function(){
+                var number = $(this).attr("id");
+                gallery(number)
+            })
         }
     })
 }
+
 function gallery(number)
 {
-
-    var a = '/food/'
     $.ajax({
-        url: a + number,
+        url: 'food/' + number,
         type : 'GET',
         success:function(data){
-                menupicture(data)
+                menuPicture(data)
+                
             }
         })
 }
@@ -29,18 +33,15 @@ function gallery(number)
 function menu(data)
 {   
     $("#menu").append(`<li id="${data.id}">${data.name}</li>`); // <ul>밑에 jquery로 <li>자식생성
-    $("#menu li").click(function(){
-        var number = $(this).attr("id");
-        gallery(number)
-    })
 }
 
 
-function menupicture(data)
+function menuPicture(data)
 {
-        $("#picture").html(`<img src = "image/${data[0].filename}" width = "350px" height = "250px">`) 
-        $("#picture").append(`<img src = "image/${data[1].filename}" width = "350px" height = "250px">`) 
-        $("#picture").append(`<img src = "image/${data[2].filename}" width = "350px" height = "250px">`) 
+        $("#picture").html(`<img src = "image/${data[0].filename}" width = "350px" height = "250px">`)
+        for(var i = 1; i < data.length; i++){
+        $("#picture").append(`<img src = "image/${data[i].filename}" width = "350px" height = "250px">`) 
+    }
 }
 
 // $("#picture").append(`<img src = "MenuList%20Picture/${data.filename}" width = "350px" height = "250px">`)
