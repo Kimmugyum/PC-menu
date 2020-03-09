32240a598b79f50362aef375140f5b0336e66cca
Kim Mugyum <gyum2726@naver.com>
2020-03-09 19:03:03 +0900

    file이름변경 및, 상품이름 및 가격 추가

--- public/script.js
+++ public/script.js
@@ -39,7 +39,12 @@ function menuPicture(data)
 {
         $("#picture").html('')
         for(var i = 0; i < data.length; i++){
-        $("#picture").append(`<div><img src = "image/${data[i].filename}" width = "350px" height = "250px">
-                                ${data[i].name}</div>`) 
+        $("#picture").append(`<div><img src = "image/${data[i].filename}" width = "400px" height = "250px">
+                            <div class = "price">${data[i].name} ${data[i].price}원</div>
+                            <div id = "all">
+                            <i class="far fa-plus-square fa-3x"></i>
+                            <input type ="text" id = "count" value = "0개">
+                            <i class="far fa-minus-square fa-3x"></i></div>
+                            </div>`)
     }
 }
