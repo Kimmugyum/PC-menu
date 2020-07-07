45ad45582c4c5e30c86f7662732a2dd15823e49a
Kim Mugyum <gyum2726@naver.com>
2020-07-08 02:21:23 +0900

    html수량 제거 및 JQuery +,-버튼 삭제 및 메뉴목록 메뉴이름 가격만 나오게 설정

--- public/script.js
+++ public/script.js
@@ -74,49 +74,23 @@ function menu(data) {   // <ul>밑에 jquery로 <li>자식생성 : 메뉴이름
     $("#menu").append(`<li id="${data.id}">${data.name}</li>`); 
 }
 
-
 function foodPicture(value) {
     var data = MenuList[value]
+    console.log(data)
         $("#picture").append(
         `<div id=${value}>
             <img src = "image/${data.filename}"} width = "400px" height = "250px">
             <div class = "price"><p>${data.name} ${data.price}원</p></div>
-                <div class = "all">
-                    <input type = "button" class = "plus" value = "+">
-                    <input type ="text" class = "count" value = 0>
-                    <input type = "button" class = "minus" value = "-">
-                </div>
         </div>`)
 }
 
-$(document).on('click','.plus', function(){
-    var productId = $(this).parent().parent().attr("id")
-    var number = $(this).next().val()
-    var changeNumber = parseInt(number)
-    $(this).next().val(changeNumber += 1)
+$(document).on('click','.price', function(){
+    var productId = $(this).parent().attr("id")
     console.log(productId)
-    var c = $("#moniter").children().attr("id")
-    console.log(c)
-    if(productId != $("#moniter").children().attr("id")){
     $("#moniter").append(`
                 <tr id = ${productId}>
                     <td class = "menusize">${MenuList[productId].name}</td>
                     <td class = "menusize">${MenuList[productId].price}</td>1
-                    <td class = "menusize" id = "a">${changeNumber}</td>
                 </tr>
         `)
-    }
-    else{
-    var tdNumber = $("#a").text()
-    var change = parseInt(tdNumber)
-    $("#a").text(change+1)
-    }
 })
-
-$(document).on('click','.minus', function(){
-    var number = $(this).prev().val()
-    if(number > 0){
-        var changeNumber = parseInt(number)
-        $(this).prev().val(changeNumber -= 1)
-    }
-})a
